16c39370c38735a98d466823e976c7a8e48755ec
HusnainSyed <daimshah784@gmail.com>
2022-05-05 13:33:11 +0500

    added env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import 'reflect-metadata';
 import path from 'path';
+import dotenv from 'dotenv';
 import { App } from './App';
 import { initialFileLoad } from './fileLoaders';
 import './controller/RootController';
 import './controller/TourController';
 
+dotenv.config({ path: path.join(__dirname, '../config.env') });
 initialFileLoad(path.join(__dirname, '../dev-data/data/tours.json'));
-App.start(3000, () => {
+const port = Number(process.env.PORT) || 3000;
+App.start(port, () => {
+	console.log(process.env.NODE_ENV);
+	console.log(process.env.PORT);
+
 	console.log('Server listening on port 3000');
 });
